ffa9dad742c035119730ee38eca60e671dfa586b
UmaanMehraj <umaanmehraj5@gmail.com>
2026-05-05 18:50:45 +0530

    small fixes

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 export default function Home() {
   return (
     <>
-      <div className='bg-gray-300 min-h-120 w-screen flex flex-col justify-center items-center'>
+      <div className='bg-gray-300 min-h-120 w-screen flex flex-col justify-center items-center '>
         <div className='h-20 text-3xl lg:text-6xl md:text-4xl m-2'>
           Shop Now
         </div>
@@ -14,7 +14,7 @@ export default function Home() {
           Shop variety of collections, shipment throughout the country
         </div>
       </div>
-      <div className='bg-gray-300 min-h-30 w-screen flex gap-4 justify-center items-center'>
+      <div className='bg-gray-300 min-h-30 w-screen flex gap-4 justify-center items-center pb-4'>
         <div className='m-1 h-8 w-20 bg-white p-4 rounded-2xl border-2 flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-black hover:text-white'>
           <Link to='/shop'>Shop</Link>
         </div>
