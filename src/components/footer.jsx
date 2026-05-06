6f29514c023b47aa8e86a7bbf7ea0c3ee5ff6da8
UmaanMehraj <umaanmehraj5@gmail.com>
2026-05-06 11:09:32 +0530

    update: footer.jsx, homepage.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,17 @@
 export default function Footer() {
-  return <></>
+  return (
+    <>
+      <footer className='flex mt-10 left-0 bottom-0 justify-center items-center text-white'>
+        <div>
+          &copy; 2026{' '}
+          <a
+            href='https://github.com/UmaanMehraj'
+            className='underline text-violet-200'
+          >
+            Umaan Mehraj
+          </a>
+        </div>
+      </footer>
+    </>
+  )
 }
